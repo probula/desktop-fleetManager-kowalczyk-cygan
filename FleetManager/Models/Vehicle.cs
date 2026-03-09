ce87ef60600814bb4fc6017e0caeb437bd3b8e22
igor <igor.kowalczyk07@poczta.fm>
2026-03-09 22:14:20 +0100

    added vehicle model and json service

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,45 @@
+using ReactiveUI;
+
 namespace FleetManager.Models;
 
-public class Vehicle
+public enum VehicleStatus
 {
-    
+    Available,
+    InRoute,
+    Service
+}
+
+
+public class Vehicle : ReactiveObject
+{
+    private string _name = string.Empty;
+    private string _licensePlate = string.Empty;
+    private double _fuelLevel;
+    private VehicleStatus _status;
+
+    public string Name 
+    { 
+        get => _name; 
+        set => this.RaiseAndSetIfChanged(ref _name, value); 
+    }
+
+    public string LicensePlate 
+    { 
+        get => _licensePlate; 
+        set => this.RaiseAndSetIfChanged(ref _licensePlate, value); 
+    }
+
+   
+    public double FuelLevel 
+    { 
+        get => _fuelLevel; 
+        set => this.RaiseAndSetIfChanged(ref _fuelLevel, value); 
+    }
+
+    //0 - available, 1 - InRoute, 2 - Service
+    public VehicleStatus Status 
+    { 
+        get => _status; 
+        set => this.RaiseAndSetIfChanged(ref _status, value); 
+    }
 }
